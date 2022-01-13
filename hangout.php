a3ce130a20ffa67de6bdc6ee9dac8172bddc8e03
Fabian (iPad) <sfabianlyl@gmail.com>
2022-01-13 14:24:29 +0800

    hangout

--- hangout.php
+++ hangout.php
@@ -756,9 +756,10 @@
                 <div class="row mb-5 align-items-center">
                     <div class="col-4">No. Telefon (Phone Number)</div>
                     <div class="col-8">
-                        <div class="d-flex">
-                            <span>+6</span>
+                        <div class="d-flex align-items-center">
+                            <span class="px-3">+6</span>
                             <div style="flex:1;"><input type="text" name="phone" class="form-control" value="" required></div>
+                        </div>
                     </div>
                 </div>
                 <div class="row mb-5 align-items-center">
